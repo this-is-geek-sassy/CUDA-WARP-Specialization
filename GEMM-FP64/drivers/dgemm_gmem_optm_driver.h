fb1ec50311ae82e044985f861cb9f13b6ec5c2f6
ChuChuCHAN-Developer-OP <developer.op@chuchuchan.in>
2025-10-17 16:27:17 +0530

    Added Global Memory Batching; Added Global Memory Chunking;

---
+++ GEMM-FP64/drivers/dgemm_gmem_optm_driver.h
@@ -0,0 +1,14 @@
+#ifndef DGEMM_GMEM_OPTM_DRIVER_H
+#define DGEMM_GMEM_OPTM_DRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool dgemm_gmem_optm_driver(int M, int N, int K, double* hA, double* hB, double* hC);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DGEMM_GMEM_OPTM_DRIVER_H
