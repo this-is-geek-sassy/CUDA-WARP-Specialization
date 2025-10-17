6131562096ed8473bb3a55c6703c117f720dc5b0
ChuChuCHAN-Developer-OP <developer.op@chuchuchan.in>
2025-10-17 18:19:48 +0530

    Added Bank Conflict resolved kernel;

---
+++ GEMM-FP64/drivers/dgemm_bank_conflicts_driver.h
@@ -0,0 +1,14 @@
+#ifndef DGEMM_BANK_CONFLICTS_DRIVER_H
+#define DGEMM_BANK_CONFLICTS_DRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool dgemm_bank_conflicts_driver(int M, int N, int K, double* hA, double* hB, double* hC);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DGEMM_BANK_CONFLICTS_DRIVER_H
--- GEMM-FP64/kernels/dgemm_bank_conflicts.cuh
+++ GEMM-FP64/kernels/dgemm_bank_conflicts.cuh
@@ -1,11 +1,11 @@
-#ifndef DGEMM_REGISTER_TILED_CUH
-#define DGEMM_REGISTER_TILED_CUH
+#ifndef DGEMM_BANK_CONFLICTS_CUH
+#define DGEMM_BANK_CONFLICTS_CUH
 
 #include <cuda.h>
 #include <cassert>
 #include "utils/global_mem_utils.cuh"
 
-/// @brief Register Tiled DGEMM Kernel
+/// @brief Bank Conflicts Free DGEMM Kernel
 /// @param BM Tile Size Dimension (compile-time constant)
 /// @param BK Tile Size Dimension (compile-time constant)
 /// @param BN Tile Size Dimension (compile-time constant)
@@ -19,13 +19,10 @@
 /// @param B Pointer to B matrix (K x N)
 /// @param C Pointer to C matrix (M x N)
 template<unsigned int BM, unsigned int BK, unsigned int BN, unsigned int TM, unsigned int TN, unsigned int TK, unsigned int NUM_THREADS>
-__global__ void dgemm_register_tiled(int M, int N, int K, double* A, double* B, double* C) {
+__global__ void dgemm_bank_conflicts(int M, int N, int K, double* A, double* B, double* C) {
   extern __shared__ double sm[];
   double* sA = &sm[0];
   double* sB = &sm[BM * BK];
-
-  constexpr unsigned int TM_STEP = BM / TM;
-  constexpr unsigned int TN_STEP = BN / TN;
 
   const unsigned int tx = threadIdx.x;
   const unsigned int ty = threadIdx.y;
@@ -43,15 +40,15 @@ __global__ void dgemm_register_tiled(int M, int N, int K, double* A, double* B, 
   for(unsigned int bk = 0; bk < K; bk += BK) {
     double* gA = A + (bm * K + bk);
     double* gB = B + (bk * N + bn);
-    tileMemcpyVectorised<BM, BK, NUM_THREADS>(gA, sA, K);
-    tileMemcpyVectorised<BK, BN, NUM_THREADS>(gB, sB, N);
+    loadTileChunked<BM, BK, NUM_THREADS>(K, gA, sA);
+    loadTileChunked<BK, BN, NUM_THREADS>(N, gB, sB);
     __syncthreads();
 
     for(int wk = 0; wk < BK; wk += TK) {
       // Tiled loads into Register Memory (Need to check PTX and SASS to confirm unrolling and chunking)
       for(int k = 0; k < TK; k++) {
-        for(int i = 0; i < TM; i++) a_reg[i][k] = sA[(ty + i * TM_STEP) * BK + wk + k];
-        for(int j = 0; j < TN; j++) b_reg[k][j] = sB[(wk + k) * BN + tx + j * TN_STEP]; 
+        for(int i = 0; i < TM; i++) a_reg[i][k] = sA[(ty * TM + i) * BK + wk + k];
+        for(int j = 0; j < TN; j++) b_reg[k][j] = sB[(wk + k) * BN + tx * TN + j];
       }
   
       // FMA operations on Register Memory (Need to check PTX and SASS to confirm unrolling)
@@ -63,9 +60,11 @@ __global__ void dgemm_register_tiled(int M, int N, int K, double* A, double* B, 
     __syncthreads();
   }
 
-  for(int i = 0; i < TM; i++) 
-    for(int j = 0; j < TN; j++) 
-      C[(bm + ty + i * TM_STEP) * N + (bn + tx + j * TN_STEP)] = acc_reg[i][j]; 
+  for(int i = 0; i < TM; i++) {
+    for(int j = 0; j < TN; j++) {
+      C[(bm + ty * TM + i) * N + (bn + tx * TN + j)] = acc_reg[i][j];
+    }
+  }
 }
 
-#endif // DGEMM_REGISTER_TILED_CUH
+#endif // DGEMM_BANK_CONFLICTS_CUH
